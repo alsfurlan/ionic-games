5546c5228cd0431caddc634c21a5d9acbe9397f5
Anderson Furlan <alsfurlan@gmail.com>
2021-11-09 20:24:15 -0300

    Excluído folder page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    // redirectTo: 'folder/Inbox',
     pathMatch: 'full'
   },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
 ];
 
 @NgModule({
